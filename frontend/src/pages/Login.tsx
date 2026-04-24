dc19bbe9dbd7d59e69762ea56e148a777f57ef39
Dedealtograu <matias.jms14@gmail.com>
2026-04-24 12:33:22 -0300

    login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -6,10 +6,39 @@ import Button from "../components/Button";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
+  async function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
     e.preventDefault();
-    console.log(email, password);
+
+    try {
+      if (!email || !password) {
+        setError("Usuário e senha são obrigatórios");
+        return;
+      }
+      const response = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.status === 404) {
+        setError("Usuário não encontrado");
+      }
+
+      if (response.status === 400) {
+        setError("Usuário e senha são obrigatórios");
+      }
+
+      if (response.status === 200) {
+        setError("");
+        const data = await response.json();
+        console.log(data);
+      }
+    } catch (erro) {
+      console.error(erro);
+      return;
+    }
   }
 
   return (
@@ -17,22 +46,26 @@ const Login = () => {
       className="flex h-screen items-center justify-center bg-[#161410]"
       onSubmit={handleSubmit}
     >
-      <div className="flex flex-col items-center justify-center gap-2">
+      <div className="flex flex-col justify-center gap-2">
         <Link to="/">
-          <img src="/logo.png" alt="" className="mb-4" />
+          <img src="/logo.png" alt="" className="mx-auto mb-4" />
         </Link>
 
-        <Input
-          placeholder="E-mail"
-          type="email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Input
-          placeholder="Senha"
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button title="Login" variant="default" />
+        <div className="mb-3 flex flex-col gap-2">
+          <Input
+            placeholder="E-mail"
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            placeholder="Senha"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <p className="text-left text-sm font-bold text-red-600">{error}</p>
+        </div>
+
+        <Button title="Login" variant="default" type="submit" />
         <Link to="/register" className="w-full">
           <Button title="Não tenho uma conta" variant="outline" />
         </Link>
